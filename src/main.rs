7154af67a2b50c189686dbb559caafaeab44440c
David Camperos <davidapks@gmail.com>
2020-03-20 22:56:09 -0400

    Spotify client access now reads from env

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use tbot::prelude::*;
 use reqwest::header::{ACCEPT, X_CONTENT_TYPE_OPTIONS, AUTHORIZATION};
+use serde::Deserialize;
+use std::env::var;
 
 struct LinkData {
     artist: String,
     track: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct SpotifyAuth {
+    access_token: String,
+    token_type: String,
+    expires_in: u32
+}
+
+struct SpotifyCredentials {
+    client_id: String,
+    client_secret: String
 }
 
 async fn deezer_search_link(song: &LinkData) -> Result<String, reqwest::Error> {
@@ -27,34 +41,61 @@ async fn deezer_search_link(song: &LinkData) -> Result<String, reqwest::Error> {
     Ok(res.to_string())
 }
 
-async fn spotify_link(link: &str) -> Result<LinkData, reqwest::Error> {
+async fn spotify_link(link: &str, credentials: &SpotifyCredentials) -> Result<LinkData, reqwest::Error> {
 
-    const API_LINK: &'static str = "https://api.spotify.com/v1/tracks/";
+    let client = reqwest::Client::new();
+
+    const DOMAIN: &'static str = "https://api.spotify.com";
+
+    const API_VERSION: &'static str = "/v1";
+
+    const API_TRACKS: &'static str = "/tracks/";
 
     const SPOTIFY_URL: &'static str = "https://open.spotify.com/track/";
 
     const SPOTIFY_ID_LEN: usize = 22;
 
+    //Auth
+    const TOKEN_URL: &'static str = "https://accounts.spotify.com/api/token";
+
+    let credentials = base64::encode(format!("{}:{}", credentials.client_id, credentials.client_secret));
+
+    let auth = format!("Basic {}", credentials);
+    let body_params = [("grant_type", "client_credentials")];
+
+    let token_res = client
+        .post(TOKEN_URL)
+        .header(AUTHORIZATION, auth)
+        .form(&body_params)
+        .send()
+        .await?
+        .json::<SpotifyAuth>()
+        .await?;
+
+    //let token_res = token_res.send().await?;
+
+    //dbg!(&token_res);
+    //End auth
+
     let (song, _) = link.trim_start_matches(SPOTIFY_URL).split_at(SPOTIFY_ID_LEN);
 
-    let link = format!("{}{}", API_LINK, song);
+    let link = format!("{}{}{}{}", DOMAIN, API_VERSION, API_TRACKS, song);
 
-    const TOKEN: &'static str = "";
-
-
-    let client = reqwest::Client::new();
+    //const TOKEN: &'static str = "";
+    
     let res = client
         .get(&link)
         .header(ACCEPT, "application/json")
         .header(X_CONTENT_TYPE_OPTIONS, "application/json")
-        .header(AUTHORIZATION, format!("Bearer {}", TOKEN))
+        .header(AUTHORIZATION, format!("Bearer {}", token_res.access_token))
         .send()
         .await?;
 
     println!("status: {}", res.status().as_str());
 
     let res = res.text().await?;
-    println!("request: {}", res);
+    //println!("request: {}", res);
+    dbg!(&res);
 
     let res: serde_json::Value = serde_json::from_str(res.as_str()).unwrap();
 
@@ -76,7 +117,18 @@ async fn main() -> Result<(), reqwest::Error> {
     bot.text(|context| {
         async move {
 
-            let spotify_data = spotify_link(&context.text.value).await.unwrap();
+            let spotify_credentials = SpotifyCredentials {
+                client_id: match var("BOT_SPOTIFY_CLIENT_ID") {
+                    Ok(var) => var,
+                    Err(_) => panic!(),
+                },
+                client_secret: match var("BOT_SPOTIFY_CLIENT_SECRET") {
+                    Ok(var) => var,
+                    Err(_) => panic!(),
+                }
+            };
+
+            let spotify_data = spotify_link(&context.text.value, &spotify_credentials).await.unwrap();
 
             let message = deezer_search_link(&spotify_data).await.unwrap();
             
